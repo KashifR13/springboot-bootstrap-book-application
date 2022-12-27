964e0d86f3104e9ee0ad26ed1b097167405b2159
Kashif Rasool <kashif.rasool@tuni.fi>
2022-12-27 13:17:38 +0200

    Persitance layers i.e. entity, repo and related configs added

--- src/main/java/com/mkr/book/BookApplication.java
+++ src/main/java/com/mkr/book/BookApplication.java
@@ -2,7 +2,11 @@ package com.mkr.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.mkr.book.repository")
+@EntityScan("com.mkr.book.entity")
 @SpringBootApplication
 public class BookApplication {
 
---
+++ src/main/java/com/mkr/book/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.mkr.book.repository;
+
+import com.mkr.book.entity.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
